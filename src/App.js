3c293e0e37c4c98eb4b49a76e19825f9a3f82a6b
“Piyush <piyush_lohani@intuit.com>
2025-05-21 16:11:47 +0530

    InitialApp2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home.jsx';
 
 
 function App() {
   return (
    <Router>
     <div>
-    <Header /></div>
+    <Header />
+    <Home />
+    </div>
   </Router>
   );
 }
