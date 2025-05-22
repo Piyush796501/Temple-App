7d3dccd527526b312727f8c1ff3f288b44175109
“Piyush <piyush_lohani@intuit.com>
2025-05-22 20:37:38 +0530

    InitialApp4

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div >
+        
         <img src={`${process.env.PUBLIC_URL}/aashram.jpg`} alt="Temple"/>
         <h1>Bhagavaan Gopinaath Ji</h1>
         <p>
@@ -12,6 +13,56 @@ function About() {
            has been described as Shambhavi avastha (state of Shiva).
           Contemporary saints of his times have also called him an Aghoreshwar. It was sometime during 1946–1956 that he came to be called as Bhagavaan Gopinath Ji by his devotees.
         </p>
+        <div className="info-section">
+  <h2>Information</h2>
+
+  <div className="info-row">
+    <div className="info-title">Dham</div>
+    <div className="info-value">
+      <button className="tag">Prayer Hall</button>
+      <button className="tag">Shri Radha Krishna</button>
+      <button className="tag">Shri Gauri Shankar</button>
+      <button className="tag">Shri Ram Pariwar</button>
+      <button className="tag">Maa Durga</button>
+      <button className="tag">Shivling with Gan</button>
+      <button className="tag">Outer Area</button>
+    </div>
+  </div>
+
+  <div className="info-row">
+    <div className="info-title">Basic Services</div>
+    <div className="info-value">
+      Prasad, Drinking Water, Water Cooler, Shoe Store, Sitting Benches, Prasad Shop, Washrooms
+    </div>
+  </div>
+
+  <div className="info-row">
+    <div className="info-title">Founder</div>
+    <div className="info-value">Kashmiri Welfare Society</div>
+  </div>
+
+  <div className="info-row">
+    <div className="info-title">Organized By</div>
+    <div className="info-value">Kashmiri Welfare Society</div>
+  </div>
+
+  <div className="info-row">
+    <div className="info-title">Dedicated To</div>
+    <div className="info-value">Shri Shiv</div>
+  </div>
+
+  <div className="info-row">
+    <div className="info-title">Photography</div>
+    <div className="info-value">
+      ✓ Yes <small>(It's not ethical to capture photographs during worship. Follow temple’s rules.)</small>
+    </div>
+  </div>
+
+  <div className="info-row">
+    <div className="info-title">Free Entry</div>
+    <div className="info-value">✓ Yes</div>
+  </div>
+</div>
     </div>
   )
 }
--- src/components/Title.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Title() {
-  return (
-    <div><h1>Sidheswar Sivalay & Gopinath Aashram </h1></div>
-  )
-}
-
-export default Title;
